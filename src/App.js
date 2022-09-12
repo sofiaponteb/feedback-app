256d26c6ee6f86353d4d31251bc23dd4cc7a6404
sofiaponteb <aaponte@unal.edu.co>
2022-09-12 18:37:20 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Header from "./components/Header"
+
 function App(){
     return (
+        <>
+        <Header />
         <div className="container">
             <h1> My App </h1>
         </div>
+        </>
     )
 }
 
