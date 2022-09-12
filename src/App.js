43c230946afbfb09b2944a402f6e6d4a9e95b3a0
sofiaponteb <aaponte@unal.edu.co>
2022-09-12 17:28:28 -0500

    listas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+function App(){
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'},
+    ]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className = "container">
+            <h1> {title.toUpperCase()} </h1>
+            <p> {body} </p>
+            {Math.random() * (5 + 5) }
+            <div className="comments"> 
+                <h3> Comments({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                        <li key={index}>{comment.text} </li>
+                    )
+
+                    )}
+                </ul>
+            </div>
+        </div>
+        
+    )
 }
 
-export default App;
+export default App
