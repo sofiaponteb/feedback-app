00d2ca0c55487c0887ad7658e1ad90379bebf709
sofiaponteb <aaponte@unal.edu.co>
2022-09-13 18:48:25 -0500

    stats component

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+function FeedbackStats({feedback}) {
+//Calculate ratings average
+let average = feedback.reduce((acc, curr) => {
+    return acc + curr.rating
+}, 0) / feedback.length 
+
+average = average.toFixed(1).replace(/[.,]0$/, '') // 1 digito decimal, si es cero se reemplaza con nada
+
+console.log(average)
+
+  return (
+    <div className="feedback-stats">
+        <h4> {feedback.length} Reviews </h4> 
+        <h4> Average Rating: {isNaN(average) ? 0 : average} </h4>
+    </div>
+  )
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired,
+}
+
+export default FeedbackStats
