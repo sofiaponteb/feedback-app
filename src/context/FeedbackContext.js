48f3b2ca536ed20b5a1b3b831218f322259eb5c7
sofiaponteb <aaponte@unal.edu.co>
2022-09-28 15:25:07 -0500

    add feedback to backend - proxy

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react"
-import {v4 as uuidv4} from 'uuid'
+//import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -20,7 +20,7 @@ export const FeedbackProvider = ({children}) => {
 
     //fetch feedback
     const fetchFeedback = async ()=> {
-        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const response = await fetch(`/feedback?_sort=id&_order=desc`)
         const data = await response.json()
 
         //console.log(data)
@@ -44,9 +44,19 @@ export const FeedbackProvider = ({children}) => {
     }
 
     //add feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+
+        //newFeedback.id = uuidv4()
+        setFeedback([data, ...feedback])
     }
 
     // set item to be updated
